b0994f97f8d76f6610cbffab3a42329f76a8ed11
DinoAsm <DinoAsm@yandex.ua>
2014-08-28 12:32:37 +0300

    modified:   cjdns/Makefile 	new file:   cjdns/install.sh Tried to move install commands outside of Makefile.

---
+++ cjdns/install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+	${INSTALL_DIR} ${1}/usr/sbin
+	${INSTALL_BIN} ${PKG_BUILD_DIR}/cjdroute   ${1}/usr/sbin/cjdroute
+	${INSTALL_DIR} ${1}/etc/init.d
+	if [ "${CONFIG_CJDNS_INIT_PROCD}" == "y" ]
+		${INSTALL_BIN} ./files/cjdns_init-procd 	${1}/etc/init.d/cjdns
+	else
+		${INSTALL_BIN} ./files/cjdns_init-oldstyle 	${1}/etc/init.d/cjdns		
+	fi
+
