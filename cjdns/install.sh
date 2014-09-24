7f189eddf0a46200e57835c984558e633a58b89f
DinoAsm <DinoAsm@yandex.ua>
2014-09-24 21:27:04 +0300

    A fix for a small bashism.

--- cjdns/install.sh
+++ cjdns/install.sh
@@ -2,7 +2,7 @@
 	${INSTALL_DIR} ${1}/usr/sbin
 	${INSTALL_BIN} ${PKG_BUILD_DIR}/cjdroute   ${1}/usr/sbin/cjdroute
 	${INSTALL_DIR} ${1}/etc/init.d
-	if [ "${CONFIG_CJDNS_INIT_PROCD}" == "y" ]
+	if [ "${CONFIG_CJDNS_INIT_PROCD}" = "y" ]
 	then		
 		${INSTALL_BIN} ./files/cjdns_init-procd 	${1}/etc/init.d/cjdns
 	else
